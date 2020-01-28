a59e2c8144cf7689482ec9aff6691443c4ce0c7a
Yuliaperegudova <yuse4ka08@gmail.com>
2020-01-28 16:49:27 +0300

    Все работает, нужно доделать цвет шрифта игроков

--- js/stat.js
+++ js/stat.js
@@ -1,44 +1,54 @@
+/* eslint-disable strict */
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 25;
-var TEXT_WIDTH = 50;
 var BAR_WIDHT = 40;
 var BAR_GAP = 50;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-  return maxElement;
-}
-
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = '#000';
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+    return maxElement;
+  };
 
   var maxTime = getMaxElement(times);
 
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP)*i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP)*i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP, BAR_WIDHT, -((BAR_HEIGHT * times[i]) / maxTime));
+  var getColor = function () {
+    if (names[i] === 'Вы') {
+      var color = 'rgba(255, 0, 0, 1)';
+    } else {
+      color = 'hsl(' + '240' + ',' + Math.floor(Math.random() * 100) + '%, ' + '50' + '%)';
+    }
+    return color;
   };
 
-  ctx.fillStyle = '#000';
-  ctx.fillText ('Ура вы победили!', 150, 50);
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = getColor();
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP, BAR_WIDHT, -((BAR_HEIGHT * Math.floor(times[i])) / maxTime));
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP - ((BAR_HEIGHT * Math.floor(times[i])) / maxTime) - GAP);
+  }
 
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 };
+
