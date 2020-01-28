be94afe09aa5da1666a38ec9660940ce8e60146a
Yuliaperegudova <yuse4ka08@gmail.com>
2020-01-28 17:11:15 +0300

    Задание выполнено

--- js/stat.js
+++ js/stat.js
@@ -3,9 +3,9 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var FONT_GAP = 25;
-var BAR_WIDHT = 40;
+var CLOUD_GAP = 10;
+var FONT_GAP = 35;
+var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var BAR_HEIGHT = 150;
 
@@ -15,7 +15,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   var getMaxElement = function (arr) {
@@ -40,10 +40,11 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   for (var i = 0; i < names.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
     ctx.fillStyle = getColor();
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP + FONT_GAP);
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP, BAR_WIDHT, -((BAR_HEIGHT * Math.floor(times[i])) / maxTime));
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDHT + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP - ((BAR_HEIGHT * Math.floor(times[i])) / maxTime) - GAP);
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP, BAR_WIDTH, -((BAR_HEIGHT * Math.floor(times[i])) / maxTime));
+    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_GAP - ((BAR_HEIGHT * Math.floor(times[i])) / maxTime));
   }
 
   ctx.fillStyle = '#000';
