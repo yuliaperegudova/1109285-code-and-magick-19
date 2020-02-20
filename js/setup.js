4209eec1a91fb5bffce85316bb93da3ec4b88da0
Yuliaperegudova <yuse4ka08@gmail.com>
2020-02-20 12:02:11 +0300

    разделилиа код на модули

--- js/setup.js
+++ js/setup.js
@@ -63,17 +63,3 @@
   window.WIZARD_EYESCOLORS = WIZARD_EYESCOLORS;
   window.FIREBALL_COLORS = FIREBALL_COLORS;
 })();
-
-(function () {
-
-  var wizardCoat = document.querySelector('.wizard-coat');
-  var coatColor = document.querySelector('input[name = coat-color]');
-  var wizardEyes = document.querySelector('.wizard-eyes');
-  var eyesColor = document.querySelector('input[name = eyes-color]');
-  var fireball = document.querySelector('.setup-fireball-wrap');
-  var fireballColor = document.querySelector('input[name = fireball-color]');
-
-  window.colorize(wizardCoat, window.WIZARD_COATCOLORS, coatColor);
-  window.colorize(wizardEyes, window.WIZARD_EYESCOLORS, eyesColor);
-  window.colorize(fireball, window.FIREBALL_COLORS, fireballColor);
-})();
